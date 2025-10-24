d5f86e235c8f03dd3c783fbcbe354d530a88e348
Benjamin Demaille <benjamin.demaille@icloud.com>
2025-10-24 13:14:35 +0200

    feat(mps): Batch 16 - Enriched Reduction ops (25+ ops)

--- tensorflow/mps/kernels/mps_logical_ops.mm
+++ tensorflow/mps/kernels/mps_logical_ops.mm
@@ -27,10 +27,148 @@ namespace mps {
 // TODO: Extract from mps_pluggable_device_plugin.mm
 
 void* MPSLogicalAnd_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
-void MPSLogicalAnd_Compute(void* kernel, TF_OpKernelContext* ctx) { /* TODO */ }
+void MPSLogicalAnd_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "LogicalAnd not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
 void MPSLogicalAnd_Delete(void* kernel) {}
 
-// ... (2 more logical ops)
+void* MPSLogicalOr_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSLogicalOr_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "LogicalOr not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSLogicalOr_Delete(void* kernel) {}
+
+void* MPSLogicalNot_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSLogicalNot_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "LogicalNot not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSLogicalNot_Delete(void* kernel) {}
+
+void* MPSLogicalXor_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSLogicalXor_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "LogicalXor not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSLogicalXor_Delete(void* kernel) {}
+
+void* MPSEqual_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSEqual_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "Equal not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSEqual_Delete(void* kernel) {}
+
+void* MPSNotEqual_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSNotEqual_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "NotEqual not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSNotEqual_Delete(void* kernel) {}
+
+void* MPSGreater_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSGreater_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "Greater not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSGreater_Delete(void* kernel) {}
+
+void* MPSGreaterEqual_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSGreaterEqual_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "GreaterEqual not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSGreaterEqual_Delete(void* kernel) {}
+
+void* MPSLess_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSLess_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "Less not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSLess_Delete(void* kernel) {}
+
+void* MPSLessEqual_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSLessEqual_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "LessEqual not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSLessEqual_Delete(void* kernel) {}
+
+void* MPSSelect_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSSelect_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "Select not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSSelect_Delete(void* kernel) {}
+
+void* MPSSelectV2_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSSelectV2_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "SelectV2 not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSSelectV2_Delete(void* kernel) {}
+
+void* MPSWhere_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSWhere_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "Where not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSWhere_Delete(void* kernel) {}
+
+void* MPSIsFinite_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSIsFinite_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "IsFinite not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSIsFinite_Delete(void* kernel) {}
+
+void* MPSIsInf_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSIsInf_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "IsInf not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSIsInf_Delete(void* kernel) {}
+
+void* MPSIsNan_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+void MPSIsNan_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "IsNan not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+void MPSIsNan_Delete(void* kernel) {}
 
 void RegisterLogicalOps(const char* platform_name, TF_Status* status) {
   // TODO: Registration
@@ -38,3 +176,4 @@ void RegisterLogicalOps(const char* platform_name, TF_Status* status) {
 
 }  // namespace mps
 }  // namespace tensorflow
+
--- tensorflow/mps/kernels/mps_reduction_ops.mm
+++ tensorflow/mps/kernels/mps_reduction_ops.mm
@@ -30,5 +30,223 @@ void RegisterReductionOps(const char* platform_name, TF_Status* status) {
   // TODO: Registration
 }
 
+// Additional reduction operations stubs
+extern "C" void* MPSReduceSum_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceSum_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceSum not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceSum_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceMean_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceMean_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceMean not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceMean_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceMax_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceMax_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceMax not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceMax_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceMin_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceMin_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceMin not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceMin_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceProd_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceProd_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceProd not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceProd_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceAll_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceAll_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceAll not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceAll_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceAny_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceAny_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceAny not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceAny_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceStd_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceStd_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceStd not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceStd_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceVariance_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceVariance_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceVariance not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceVariance_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceEuclideanNorm_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceEuclideanNorm_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceEuclideanNorm not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceEuclideanNorm_Delete(void* kernel) {}
+
+extern "C" void* MPSReduceLogsumexp_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSReduceLogsumexp_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "ReduceLogsumexp not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSReduceLogsumexp_Delete(void* kernel) {}
+
+extern "C" void* MPSCumSum_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSCumSum_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "CumSum not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSCumSum_Delete(void* kernel) {}
+
+extern "C" void* MPSCumProd_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSCumProd_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "CumProd not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSCumProd_Delete(void* kernel) {}
+
+extern "C" void* MPSCumMax_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSCumMax_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "CumMax not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSCumMax_Delete(void* kernel) {}
+
+extern "C" void* MPSCumMin_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSCumMin_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "CumMin not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSCumMin_Delete(void* kernel) {}
+
+extern "C" void* MPSSegmentSum_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSSegmentSum_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "SegmentSum not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSSegmentSum_Delete(void* kernel) {}
+
+extern "C" void* MPSSegmentMean_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSSegmentMean_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "SegmentMean not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSSegmentMean_Delete(void* kernel) {}
+
+extern "C" void* MPSSegmentMax_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSSegmentMax_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "SegmentMax not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSSegmentMax_Delete(void* kernel) {}
+
+extern "C" void* MPSSegmentMin_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSSegmentMin_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "SegmentMin not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSSegmentMin_Delete(void* kernel) {}
+
+extern "C" void* MPSSegmentProd_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSSegmentProd_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "SegmentProd not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSSegmentProd_Delete(void* kernel) {}
+
+extern "C" void* MPSUnsortedSegmentSum_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSUnsortedSegmentSum_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "UnsortedSegmentSum not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSUnsortedSegmentSum_Delete(void* kernel) {}
+
+extern "C" void* MPSUnsortedSegmentMax_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSUnsortedSegmentMax_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "UnsortedSegmentMax not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSUnsortedSegmentMax_Delete(void* kernel) {}
+
+extern "C" void* MPSUnsortedSegmentMin_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSUnsortedSegmentMin_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "UnsortedSegmentMin not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSUnsortedSegmentMin_Delete(void* kernel) {}
+
+extern "C" void* MPSUnsortedSegmentProd_Create(TF_OpKernelConstruction* ctx) { return nullptr; }
+extern "C" void MPSUnsortedSegmentProd_Compute(void* kernel, TF_OpKernelContext* ctx) {
+  TF_Status* s = TF_NewStatus();
+  TF_SetStatus(s, TF_UNIMPLEMENTED, "UnsortedSegmentProd not implemented for MPS");
+  TF_OpKernelContext_Failure(ctx, s);
+  TF_DeleteStatus(s);
+}
+extern "C" void MPSUnsortedSegmentProd_Delete(void* kernel) {}
+
 }  // namespace mps
 }  // namespace tensorflow
+
